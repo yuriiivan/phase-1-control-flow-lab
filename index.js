af8f2a869251598fa0c8267d573d6c32135998e6
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-06 10:25:17 -0400

    Passed all tests

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return 'This one is on me!'
+  } else if(feet >400 && feet < 2000){
+    return 'That will be twenty bucks.';
+  } else if(feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if(feet > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
 }
